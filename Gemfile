da4faf21b4e064de34204f64de8c9aaf36aac2a4
Joe Hainline <joe.hainline@asolutions.com>
2012-12-12 11:12:58 -0600

    testing in progress on label state history

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'sinatra_auth_github'
 gem 'sinatra-content-for', "~> 0.1.0"
 gem 'stint', :path => "./stint/stint"
 gem 'ghee'
+gem "rspec", "~> 2.12.0"
 
 group :development do
   gem 'watchr'
--- spec/label_state_history_spec.rb
+++ spec/label_state_history_spec.rb
@@ -10,8 +10,51 @@ describe LabelStateHistory do
     end
   end
 
-  describe 'serializing input to JSON' do
-    subject { described_class }
+  describe 'deserializing input from JSON' do
+    json_lsh = "<!--- { 'label_state_history': [{ \"40\" : \"2012-12-05 17:53:30 +0000\" }] } -->"
+    subject { described_class.new(json_lsh) }
+    
+    it "has a history array with one element equal to 40" do
+      subject.history_array.count.should == 1
+      subject.current_state_index.should == 40
+    end
   end
-  describe 'de-serializing input from JSON'
+  
+  describe 'serializing input to JSON' do
+    let(:expected_json) do
+      "{ 'label_state_history': [{ \"10\" : \"#{mock_time.to_s}\" }] }"
+    end
+
+    let(:mock_time) { mock 'time' }
+    let(:current_index) { 10 }
+    
+    subject { described_class.new }
+    
+    before do
+      Time.stub(:now) { mock_time }
+      subject.record_state(current_index)
+    end
+    
+    it "can be deserialized and reserialized" do
+      subject.to_json.should == expected_json
+    end
+  end
+ 
+  describe 'getting the body with state history' do
+    subject { described_class.get_body_with_state_history(old_body, index) }
+  
+    let(:index) { 42 }
+  
+    context 'a body with existing label state history that needs to be updated' do
+      let(:old_body) do %{ <!---
+{ 'label_state_history': [{ "40" : "2012-12-05 17:53:30 +0000" },{ "50" : "2012-12-11 15:57:43 +0000" }] }
+-->' }
+end
+
+      
+    end
+    
+    context 'a body with existing label state history that does not need to be updated'
+    context 'no label state history'
+  end 
 end
